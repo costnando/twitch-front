5b9ca6f3a3217722ac1b412a0083527d8814d46f
Fernando Costa <fernando@nightweb.com.br>
2019-11-20 00:54:39 -0300

    Enviando projeto para o github

---
+++ _assets/js/main.js
@@ -0,0 +1,11 @@
+// More
+$('header .more').on('click', function(){
+	$(this).toggleClass('active');
+});
+
+
+// Search Responsive
+$('.search-responsive').on('click', function(){
+	$('.box-responsive').toggleClass('active');
+});
+
